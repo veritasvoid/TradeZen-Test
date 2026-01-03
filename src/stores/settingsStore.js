9f5a0ddefc63524422e33791fbbc063509aed7b1
veritasvoid <veritasvoid2025@gmail.com>
2026-01-03 07:18:53 -0700

    Update settingsStore.js

--- src/stores/settingsStore.js
+++ src/stores/settingsStore.js
@@ -1,28 +1,26 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 import { DEFAULT_SETTINGS } from '@/lib/constants';
 
-export const useSettingsStore = create(
-  persist(
-    (set) => ({
-      settings: DEFAULT_SETTINGS,
-      
-      // Update settings
-      updateSettings: (newSettings) => {
-        set((state) => ({
-          settings: { ...state.settings, ...newSettings }
-        }));
-      },
+export const useSettingsStore = create((set) => ({
+  settings: DEFAULT_SETTINGS,
+  
+  // Update settings
+  updateSettings: (newSettings) => {
+    set((state) => ({
+      settings: { ...state.settings, ...newSettings }
+    }));
+  },
 
-      // Get currency symbol
-      getCurrencySymbol: () => {
-        const state = useSettingsStore.getState();
-        return state.settings.currency;
-      }
-    }),
-    {
-      name: 'tradezen-settings', // localStorage key
-      version: 1
-    }
-  )
-);
+  // Toggle privacy mode
+  togglePrivacyMode: () => {
+    set((state) => ({
+      settings: { ...state.settings, privacyMode: !state.settings.privacyMode }
+    }));
+  },
+
+  // Get currency symbol
+  getCurrencySymbol: () => {
+    const state = useSettingsStore.getState();
+    return state.settings.currency;
+  }
+}));
