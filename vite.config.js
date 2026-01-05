163ba29ec3ec8e9fecf7f141534311fef1547aa7
veritasvoid <veritasvoid2025@gmail.com>
2026-01-05 10:25:40 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/TradeZen/',
+  base: '/TradeZen-Test/',
   plugins: [
     react(),
     VitePWA({
